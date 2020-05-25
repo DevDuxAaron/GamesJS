33b960794016bf2c3b18a5569f9fe753b94e6cb8
Aaron Joel <joe.lim.42.54@gmail.com>
2020-05-25 12:32:55 -0400

    Logic RPS

--- Games/PiedraPapelTijera/logic.js
+++ Games/PiedraPapelTijera/logic.js
@@ -1,24 +1,39 @@
 const userScore = 0;
 const computerscore = 0;
-const UserScore_span = document.getElementById('user-score');
+const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board")
 const result_div = document.querySelector(".result")
 const rock_div = document.getElementById("r")
 const paper_div = document.getElementById("p")
-const scissors = document.getElementById("s")
+const scissors_div = document.getElementById("s")
 
 function getComputerChoice() {
     const choices = ['r','p','s'];
     const randomNumber = Math.floor(Math.random()*3);
     return choices[randomNumber]
-
 }
 
 
 function game(userChoice) {
     const computerChoice = getComputerChoice();
-    
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            console.log('I win')
+            break
+        case "rp":
+        case "ps":
+        case "sr":
+            console.log('Computer wins')
+            break
+        case "rr":
+        case "pp":
+        case "ss":
+            console.log('Draw')
+            break
+    }
 }
 
 function main() {
